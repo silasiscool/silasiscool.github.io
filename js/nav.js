7f8dc286f6e43583ac30dc5750fc7fb944c1d25b
silasiscool <92451943+silasiscool@users.noreply.github.com>
2023-01-10 09:38:44 -0500

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+// js file which adds the html for the main nav to ensure consistent page top throughout the site
+
+// get main nav and store as an element
+const mainNav = document.getElementById('main-nav');
+
+// function to add nav link
+function addNavItem(text, link) {
+  // create anchor element as navItem
+  let navItem = document.createElement('a');
+  // give navItem innerHTML, href, and classes
+  navItem.innerHTML = text;
+  navItem.href = link;
+  navItem.classList.add('main-nav-item');
+  // append navItem to mainNav
+  mainNav.appendChild(navItem);
+};
+
+// add each nav link
+addNavItem('Home','index.html');
+addNavItem('About Me', 'about-me.html');
+addNavItem('Projects','projects.html');
